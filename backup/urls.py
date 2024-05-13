661ae945b3163fc23485f8ccef24f11ec68d5e61
EdoDelle05 <edoardo.delleva@gmail.com>
2024-05-13 21:54:53 +0200

    datatables backend

--- backup/urls.py
+++ backup/urls.py
@@ -21,9 +21,9 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', users_views.register, name="register"),
+    path('register/', users_views.register, name="register"),
     path('profile/', users_views.profile, name='profile'),   
     path('login/', auth_views.LoginView.as_view(template_name= 'users/login.html'), name="login"),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
-    path('libreria/', include('libreria.urls')),
+    path('', include('libreria.urls')),
 ]
