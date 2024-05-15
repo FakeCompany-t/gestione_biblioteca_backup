55dab611840a717336c931e36ff646bef76ede28
GitCr4zy <loryxtel@gmail.com>
2024-05-15 22:34:24 +0200

    create/detail/delete

--- libreria/urls.py
+++ libreria/urls.py
@@ -1,6 +1,15 @@
 from django.urls import path,include
+from django.conf import settings
+from django.conf.urls.static import static
+from .views import LibroCreateView, LibroDetailView, LibroDeleteView
 from . import views
 
 urlpatterns = [
     path('', views.home, name="libreria-home"),
+    path('libro/new/', LibroCreateView.as_view(), name="libro-create"),
+    path('libro/<int:pk>/', LibroDetailView.as_view(), name="libro-detail"),
+    path('libro/<int:pk>/delete/', LibroDeleteView.as_view(), name="libro-delete"),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
