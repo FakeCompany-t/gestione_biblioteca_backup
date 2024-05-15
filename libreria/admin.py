35290b3b6915ee5bc7c525736e8b6f8653b0da34
EdoDelle05 <edoardodelleva@gmail.com>
2024-05-15 21:52:31 +0200

    DB fieldlibro BD Lingua

--- libreria/admin.py
+++ libreria/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from .models import Libro, Autore, LibroAutore , Casa_editrice
+from .models import Libro, Autore, LibroAutore , Casa_editrice , Lingua
 
 admin.site.register(Libro)
 admin.site.register(Autore)
 admin.site.register(Casa_editrice)
 admin.site.register(LibroAutore)
-
+admin.site.register(Lingua)
--- libreria/models.py
+++ libreria/models.py
@@ -2,6 +2,12 @@ from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
 from datetime import datetime
 
+class Lingua(models.Model):
+    lang_code = models.CharField(max_length=100)
+
+
+    def __str__(self):
+        return f"{self.lang_code}"
 
 class Autore(models.Model):
     nome = models.CharField(max_length=100)
@@ -25,6 +31,9 @@ class Libro(models.Model):
     description = models.TextField()
     data_publication = models.IntegerField(validators=[MinValueValidator(1000), MaxValueValidator(datetime.now().year)])
     casa_editrice = models.ForeignKey(Casa_editrice , on_delete=models.PROTECT, default=None)
+    lingua = models.ForeignKey(Lingua , on_delete=models.PROTECT, default=None)
+    isbn = models.IntegerField(null=True, default=0)
+    disponibile = models.BooleanField(default=False) 
 
     def __str__(self):
         return self.title
