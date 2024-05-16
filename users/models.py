bcdb2f851ba36508925330a3f302564495e7317c
EdoDelle05 <edoardodelleva@gmail.com>
2024-05-16 21:41:47 +0200

    IMGprofilefix

--- users/models.py
+++ users/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from PIL import Image
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -9,8 +10,10 @@ class Profile(models.Model):
         ('bibliotecario', 'Bibliotecario'),
         ('segreteria', 'Segreteria'),
         ('gestore_comodato', 'Gestore Comodato d\'Uso'),
-        ('amministratore', 'amministratore'),
+        ('amministratore', 'Amministratore'),
 
     ]
     ruolo = models.CharField(max_length=20, choices=RUOLO_CHOICES)
+    image = models.ImageField(default='profile_default.png', upload_to='profile_pics')
+
 
